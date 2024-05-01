8397dfafb03f5718e92d4c90ba899e6863fcd80f
Andy <chenyu77177@gmail.com>
2024-05-01 23:56:49 +0800

    新增套件 bootstrap-icons 將 bootstrap js 加入至專案

--- vue-SocialMedia/src/main.js
+++ vue-SocialMedia/src/main.js
@@ -4,8 +4,11 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
 import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap/dist/js/bootstrap.min.js'
+import 'bootstrap-icons/font/bootstrap-icons.min.css'
 import store from './store'
 import VueCookies from 'vue-cookies'
+
 
 const app = createApp(App)
 
